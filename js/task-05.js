8491ea46e673f5ec60e4fe5c02adf56c13f69f1e
Kostya2700 <103896768+Kostya2700@users.noreply.github.com>
2022-08-10 16:29:44 +0300

    tasks5

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,11 @@
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
+function onChangeInput(event) {
+  const valeuInput = event.currentTarget.value;
+  spanEl.textContent = valeuInput;
+  if (valeuInput === "") {
+    return (spanEl.textContent = "Anonymous");
+  }
+}
+inputEl.addEventListener("input", onChangeInput);
--- js/task-08.js
+++ js/task-08.js
@@ -3,3 +3,15 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+const form = document.querySelector(".login-form");
+console.log(form);
+form.addEventListener("submit", onFormSub);
+function onFormSub(event) {
+  event.preventDefault();
+  console.dir(event.currentTarget.elements);
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((email, value) => {
+    console.log("name", email);
+    // console.log("value", value);
+  });
+}
