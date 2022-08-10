d6060e5dc35b09212b6019ada4ddad473ba2232f
Kostya2700 <103896768+Kostya2700@users.noreply.github.com>
2022-08-10 13:56:58 +0300

    tasks6

--- js/task-04.js
+++ js/task-04.js
@@ -1,23 +1,19 @@
-// let counterValue = 0;
-// const elemDecrement = document.querySelector('[data-action="decrement"]');
-// const value = document.querySelector("#value");
-
-// const handleClickDecrement = () => {
-//   counterValue -= 1;
-//   value.innerHTML = counterValue;
-// };
-
-// elemDecrement.addEventListener("click", handleClickDecrement);
-
-// const elemIncrement = document.querySelector('[data-action="increment"]');
-
-// const handleClickIncrement = () => {
-//   counterValue += 1;
-//   value.innerHTML = counterValue;
-// };
-
-// elemIncrement.addEventListener("click", handleClickIncrement);
 // Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+const elemDecrement = document.querySelector('[data-action="decrement"]');
+const elemIncrement = document.querySelector('[data-action="increment"]');
+const elemValue = document.querySelector("#value");
+let counterValue = 0;
+const onDecrement = () => {
+  counterValue -= 1;
+  elemValue.innerHTML = counterValue;
+};
+elemDecrement.addEventListener("click", onDecrement);
+
+const onIncrement = () => {
+  counterValue += 1;
+  elemValue.innerHTML = counterValue;
+};
+elemIncrement.addEventListener("click", onIncrement);
--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,16 @@
-const bodyBackground = document.querySelector("body");
-
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+const bodyEl = document.querySelector("body");
+const bthEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-bodyBackground.style.backgroundColor = getRandomHexColor();
+console.log(getRandomHexColor);
+const onBodyColor = () => {
+  const colorRandom = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorRandom;
+  spanEl.innerHTML = colorRandom;
+};
+bthEl.addEventListener("click", onBodyColor);
