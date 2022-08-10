dbaaa7b486934ed9de3578a9cf63e1b8b6ebb090
Kostya2700 <103896768+Kostya2700@users.noreply.github.com>
2022-08-10 21:43:55 +0300

    hw9

--- js/task-06.js
+++ js/task-06.js
@@ -2,3 +2,12 @@
 // Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
 // Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+const inputEl = document.querySelector("#validation-input");
+function onChangeInput(event) {
+  inputEl.classList.forEach((item) => inputEl.classList.remove(item));
+  inputEl.value.length != inputEl.getAttribute("data-length")
+    ? inputEl.classList.add("invalid")
+    : inputEl.classList.add("valid");
+}
+
+inputEl.addEventListener("blur", onChangeInput);
--- js/task-08.js
+++ js/task-08.js
@@ -3,15 +3,21 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
-const form = document.querySelector(".login-form");
-console.log(form);
-form.addEventListener("submit", onFormSub);
-function onFormSub(event) {
+
+const elemSubmit = document.querySelector(".login-form");
+const userData = {};
+const onSubmit = (event) => {
   event.preventDefault();
-  console.dir(event.currentTarget.elements);
-  const formData = new FormData(event.currentTarget);
-  formData.forEach((email, value) => {
-    console.log("name", email);
-    // console.log("value", value);
-  });
-}
+  if (!elemSubmit.elements.email.value || !elemSubmit.elements.password.value) {
+    alert("Email or Password is empty!");
+    return false;
+  }
+
+  userData[elemSubmit.elements.email.name] = elemSubmit.elements.email.value;
+  userData[elemSubmit.elements.password.name] =
+    elemSubmit.elements.password.value;
+  console.log(userData);
+
+  elemSubmit.reset();
+};
+elemSubmit.addEventListener("submit", onSubmit);
